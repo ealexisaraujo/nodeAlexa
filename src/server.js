3847dcfbeafae3fac87c209ac6ff9b95901736eb
Alexis Araujo <ala1289@gmail.com>
2020-04-30 20:09:34 -0500

    Change config and run server in dev environment

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const { config } = require('./config/index');
-if (process.env.NODE_ENV !== 'production') require('morgan').morgan();
+const { config } = require('../config/index');
 
 app.use(bodyParser.json());
 
